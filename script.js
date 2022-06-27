462f80d9d1b2b024b8426867174bfa2919a5ee6d
Eimantas <eim.kasp@gmail.com>
2022-06-27 20:04:44 +0300

    Updates for top words

--- script.js
+++ script.js
@@ -6,31 +6,30 @@ var flowerCountRob = 0;
 var emoji = "\u00f0\u009f\u008c\u00b9";
 var loveDictionary = {};
 
-for (i = 0; i < messageData.messages.length; i++) {
-    console.log(i);
 
+var totalPictures = 1528; // Static value
+var totalVideos = 122; // Static value
+var totalVideos = 215; // Static value
+var totalVideos = 215; // Static value
+
+for (i = 0; i < messageData.messages.length; i++) {
     var message = messageData.messages[i];
 
     if (message.content) {
-        console.log(message);
-
-        if(message.content.includes(emoji)) {
-            console.log("Includes");
+        if (message.content.includes(emoji)) {
             flowerCount++;
             // Count EIM flowers
-            if(message.sender_name.includes('Eimantas')) {
+            if (message.sender_name.includes('Eimantas')) {
                 flowerCountEim++;
             }
-            if(message.sender_name.includes('Roberta')) {
+            if (message.sender_name.includes('Roberta')) {
                 flowerCountRob++;
             }
         }
 
-
         // Make top words dictionary
 
         var words = message.content.trim().split(/\s+/);
-        console.log(words);
 
         for (let j = 0; j < words.length; j++) {
             var word = words[j].toLowerCase();
@@ -50,21 +49,25 @@ console.log("Total EIM Count" + flowerCountEim);
 console.log("Total Rob Count" + flowerCountRob);
 
 
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     document.querySelector("#total-flowers").innerHTML = flowerCount;
     document.querySelector("#total-flowers-eim").innerHTML = flowerCountEim;
     document.querySelector("#total-flowers-rob").innerHTML = flowerCountRob;
+
+    var arr = sortObject(loveDictionary);
+    console.log(arr);
+
+    var wordList = document.querySelector("#top-words");
+    for(let l = 0; l < 50; l++) {
+        wordList.innerHTML += "<li class='p-3'>" + arr[l].key + "(" +  arr[l].value + ")</li>";
+    }
 });
 
 /* End statistics */
 
 /* Refference: https://stackoverflow.com/a/68523626/1028193 */
 // if keys are numbers
-var arr = sortObject(loveDictionary);
-console.log(arr);
-
-
-
 
 /* Reference: https://stackoverflow.com/questions/1069666/sorting-object-property-by-values */
 function sortObject(obj) {
